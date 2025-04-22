c225a754f2f1b492f318a333d39c1b5cdf84ac97
Treat William <treatwilliam.almonia1@gmail.com>
2025-04-22 23:05:27 +0800

    feat: updated layout and views

--- src/views/auth/loginView.vue
+++ src/views/auth/loginView.vue
@@ -1,65 +1,41 @@
 <script setup>
-import { ref } from 'vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useDisplay } from 'vuetify'
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="grey-lighten-1">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-10">
+          <v-card class="mx-auto" prepend-icon="mdi-account" subtitle="Login" elevation="24">
+            <template #title>
+              <span class="font-weight-black">Leak Alert</span>
+            </template>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-10">
-              <v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Login" elevation="24">
-                <template v-slot:title>
-                  <span class="font-weight-black">Leak Alert</span>
-                </template>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-login"
+                  >login</v-btn
+                >
+              </v-form>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
+              <v-divider class="my-5"></v-divider>
 
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-
-                  <v-divider class="my-5"></v-divider>
-
-                  <h5 class="text-center">
-                    Dont have an account?
-                    <RouterLink class="text-primary" to="/register"
-                      >Click here to Register</RouterLink
-                    >
-                  </h5>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="grey-lighten-1" border app>2025 - Sherlix Meatshop</v-footer>
-    </v-app>
-  </v-responsive>
+              <h5 class="text-center">
+                Dont have an account?
+                <RouterLink class="text-primary" to="/register">Click here to Register</RouterLink>
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
